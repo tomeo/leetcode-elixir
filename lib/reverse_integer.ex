ad9568782ffd4ddc379414cc5553cf8125902b47
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2022-05-29 17:13:15 +0200

    Format

--- lib/reverse_integer.ex
+++ lib/reverse_integer.ex
@@ -1,20 +1,22 @@
 defmodule ReverseInteger do
   @spec reverse(x :: integer) :: integer
   def reverse(x) do
-    multiple = case x do
+    multiple =
+      case x do
         n when n < 0 -> -1
         _ -> 1
-    end
+      end
 
-    s = x |> abs |> to_string |> String.reverse
+    s = x |> abs |> to_string |> String.reverse()
     res = multiple * String.to_integer(s)
 
     min = -2 ** 31
     max = 2 ** 31 - 1
+
     case res do
-        n when n < min -> 0
-        n when n > max -> 0
-        _ -> res
+      n when n < min -> 0
+      n when n > max -> 0
+      _ -> res
     end
   end
 end
