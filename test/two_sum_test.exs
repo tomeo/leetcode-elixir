49463fe86dd154eaa3798fff24c9e3071ed0d311
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2022-05-29 17:14:02 +0200

    Format

--- test/two_sum_test.exs
+++ test/two_sum_test.exs
@@ -3,8 +3,8 @@ defmodule TwoSumTest do
   doctest ValidParentheses
 
   test "Examples" do
-    assert TwoSum.two_sum([2,7,11,15], 9) === [0,1]
-    assert TwoSum.two_sum([3,2,4], 6) === [1,2]
-    assert TwoSum.two_sum([3,3], 6) === [0,1]
+    assert TwoSum.two_sum([2, 7, 11, 15], 9) === [0, 1]
+    assert TwoSum.two_sum([3, 2, 4], 6) === [1, 2]
+    assert TwoSum.two_sum([3, 3], 6) === [0, 1]
   end
 end
